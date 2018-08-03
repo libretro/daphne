e20373ca7945d1d643862b09b12b51190eb2b1d3
twinaphex <libretro@gmail.com>
2018-08-03 04:56:30 +0200

    Get rid of LoggerFactory

--- daphne/daphne-1.0-src/game/test_sb.cpp
+++ daphne/daphne-1.0-src/game/test_sb.cpp
@@ -13,8 +13,7 @@ test_sb::test_sb()
 
 void test_sb::start()
 {
-	ILogger *pLogger = LoggerFactory::GetInstance(LoggerFactory::NULLTYPE);
-	IScoreboard *pScoreboard = ScoreboardCollection::GetInstance(pLogger);
+	IScoreboard *pScoreboard = ScoreboardCollection::GetInstance(m_pLogger);
 	unsigned int which = IScoreboard::PLAYER1_0;
 
 	// this is mainly used to test the hardware scoreboard, so we'll add an image and hardware
@@ -44,5 +43,4 @@ void test_sb::start()
 	}
 
 	pScoreboard->PreDeleteInstance();
-	pLogger->DeleteInstance();
 }
